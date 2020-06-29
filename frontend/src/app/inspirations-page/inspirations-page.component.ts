ff457eb524937040b52262cd0a57409ca2dfaeea
Anton Milyaev <milyaev.dev@gmail.com>
2020-06-29 17:16:54 +0300

    added files

---
+++ frontend/src/app/inspirations-page/inspirations-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inspirations-page',
+  templateUrl: './inspirations-page.component.html',
+  styleUrls: ['./inspirations-page.component.styl']
+})
+export class InspirationsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
